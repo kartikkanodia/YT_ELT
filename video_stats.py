95f4ae62f7e5741ef0cf5d25667b2ff8c912a205
Kartik Kanodia <kartikkanodia@Kartiks-MacBook-Air.local>
2026-03-01 23:53:14 +0530

    first commit

--- video_stats.py
+++ video_stats.py
@@ -1,1 +1,15 @@
-print('Hello World')
+import requests
+import json
+
+API_KEY = "AIzaSyBzKLTazA-hJS0ZiXp9LJFjTjyOF-0MqKI"
+CHANNEL_HANDLE = "MrBeast"
+
+url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+response = requests.get(url)
+
+print(response)
+
+data = response.json()
+
+print(json.dumps(data, indent=4))
